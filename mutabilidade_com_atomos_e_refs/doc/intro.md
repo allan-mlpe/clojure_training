05a8508eb2f02deddc3d634c45349009e2878f88
allan-mlpe <allan_mlpe@hotmail.com>
2021-08-24 12:12:23 -0300

    Commit inicial do módulo

---
+++ mutabilidade_com_atomos_e_refs/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mutabilidade_com_atomos_e_refs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ mutabilidade_com_atomos_e_refs/test/mutabilidade_com_atomos_e_refs/core_test.clj
@@ -0,0 +1,7 @@
+(ns mutabilidade-com-atomos-e-refs.core-test
+  (:require [clojure.test :refer :all]
+            [mutabilidade-com-atomos-e-refs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
