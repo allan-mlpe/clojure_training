c899f57c80f3688a27ad2157795f41e9fe264820
allan-mlpe <allan_mlpe@hotmail.com>
2021-08-28 16:53:41 -0300

    Usando o `partial` para preparar funções que serão chamadas mais tarde

---
+++ mutabilidade_com_atomos_e_refs/src/mutabilidade_com_atomos_e_refs/aula4.clj
@@ -0,0 +1,72 @@
+(ns mutabilidade-com-atomos-e-refs.aula4
+  (:use [clojure pprint])
+  (:require [mutabilidade-com-atomos-e-refs.model :as h.model]
+            [mutabilidade-com-atomos-e-refs.logic :as h.logic]))
+
+(defn chega-em-teste!
+  [hospital departamento pessoa]
+  (swap! hospital h.logic/chega-em departamento pessoa))
+
+
+(defn simula-um-dia-lazy []
+  (let [hospital (atom (h.model/novo-hospital))
+        pessoas ["1" "2" "3" "4" "5"]
+        chega-na-fila #(.start (Thread. (fn [] (chega-em-teste! hospital :espera %))))]
+
+    ; como `map` ele só invocará a função quando precisarmos.
+    ; nesse caso, ao imprimirmos o hospital, nenhuma mudança
+    ; terá sido feita, já que não precisamos dos valores do map
+    (map chega-na-fila pessoas)
+    (pprint hospital)))
+
+
+;(simula-um-dia-lazy)
+
+(defn simula-um-dia-mapv []
+  (let [hospital (atom (h.model/novo-hospital))
+        pessoas ["1" "2" "3" "4" "5"]
+        starta-thread-entrada #(.start (Thread. (fn [] (chega-em-teste! hospital :espera %))))]
+
+    ; o `mapv` irá forçar a criação de um vetor
+    ; requerendo os valores do map.
+    ; dessa forma nós forçamos que a função seja executada
+    (mapv starta-thread-entrada pessoas)
+
+    (pprint hospital)))
+
+;(simula-um-dia-mapv)
+
+(defn starta-thread-de-entrada
+  [hospital pessoa]
+  (.start (Thread. (fn [] (chega-em-teste! hospital :espera pessoa)))))
+
+; essa função age como um `partial` implementado na mão
+(defn prepara
+  [hospital]                                                ; recebemos primeiramente o hospital
+  (fn [pessoa] (starta-thread-de-entrada hospital pessoa))) ; retornamos uma função que recebe o segundo parâmetro (`pessoa`) e faz a chamada para a função que starta a thread
+
+(defn simula-um-dia-com-funcao-de-preparacao-manual []
+  (let [hospital (atom (h.model/novo-hospital))
+        pessoas ["1" "2" "3" "4" "5"]]
+
+    ; o `mapv` irá forçar a criação de um vetor
+    ; requerendo os valores do map.
+    ; dessa forma nós forçamos que a função seja executada
+    (mapv (prepara hospital) pessoas)
+
+    (pprint hospital)))
+
+;(simula-um-dia-com-funcao-de-preparacao-manual)
+
+(defn simula-um-dia-com-partial []
+  (let [hospital (atom (h.model/novo-hospital))
+        pessoas ["1" "2" "3" "4" "5"]]
+
+    ; o `mapv` irá forçar a criação de um vetor
+    ; requerendo os valores do map.
+    ; dessa forma nós forçamos que a função seja executada
+    (mapv (partial starta-thread-de-entrada hospital) pessoas)
+
+    (pprint hospital)))
+
+(simula-um-dia-com-partial)
