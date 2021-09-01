29cc75e01d8b95de82ec3e46b141d4985ca2c8af
allan-mlpe <allan_mlpe@hotmail.com>
2021-09-01 17:01:44 -0300

    Utilizando `defmulti` e `defmethod` para estender comportamentos com estratégia

---
+++ record_protocol_multi_method/src/hospital/aula4.clj
@@ -0,0 +1,121 @@
+(ns hospital.aula4
+  (:use clojure.pprint))
+
+(defrecord PacienteParticular [id, nome, situacao, nascimento])
+(defrecord PacientePlanoDeSaude [id, nome, nascimento, situacao, plano])
+
+(defprotocol Cobravel
+  (deve-assinar-pre-autorizacao? [paciente procedimento valor]))
+
+(defn nao-eh-urgente? [paciente]
+  (not= :urgente (get paciente :situacao)))
+
+(extend-type PacienteParticular
+  Cobravel
+  (deve-assinar-pre-autorizacao? [paciente _ valor]
+    (and (>= valor 50) (nao-eh-urgente? paciente))))
+
+(extend-type PacientePlanoDeSaude
+  Cobravel
+  (deve-assinar-pre-autorizacao?
+    [paciente procedimento _]
+    (let [plano (:plano paciente)]
+      (and (not (some #(= % procedimento) plano)) (nao-eh-urgente? paciente)))))
+
+(let [paciente-particular (->PacienteParticular 1 "Bob" :normal "11/11/11")
+      paciente-plano      (->PacientePlanoDeSaude 2 "João" "10/11/12" :urgente [:raio-x :sangue])]
+
+  (println "### Estendendo com records e protocols ###")
+  (println (deve-assinar-pre-autorizacao? paciente-particular :sangue 30))
+  (println (deve-assinar-pre-autorizacao? paciente-particular :ultrassom 500))
+  (println (deve-assinar-pre-autorizacao? paciente-plano :ultrassom 500))
+  (println (deve-assinar-pre-autorizacao? paciente-plano :raio-x 700)))
+
+; função com várias implementações
+;; No caso abaixo, quando chamarmos a função
+;; `deve-assinar-pre-autorizacao-multi?` com qualquer parâmetro
+;; ela irá retornar a classe, invocando a função `class`.
+;;
+;; Então, haverá a tentativa de chamar a função com o tipo
+;; retornado. Caso não haja nenhum `defmethod` associado ao
+;; tipo retornado, então uma execeção será lançada. Caso contrário
+;; a função é chamada para o tipo em questão.
+(defmulti deve-assinar-pre-autorizacao-multi? class)        ; `class` é a função que define qual estratégia será chamada
+
+; quando chamarmos a função a seguir com uma instância
+; de `PacienteParticular` é ela que será executada
+(defmethod deve-assinar-pre-autorizacao-multi? PacienteParticular [paciente]
+  (println "invocando paciente particular")
+  true)
+
+; quando chamarmos a função a seguir com uma instância
+; de `PacientePlanoDeSaude` é ela que será executada
+(defmethod deve-assinar-pre-autorizacao-multi? PacientePlanoDeSaude [paciente]
+  (println "invocando paciente plano")
+  false)
+
+(let [paciente-particular (->PacienteParticular 1 "Bob" :normal "11/11/11")
+      paciente-plano      (->PacientePlanoDeSaude 2 "João" "10/11/12" :urgente [:raio-x :sangue])]
+
+  (println "\n\n\n### Estendendo com defmulti e defmethod ###")
+  (println (deve-assinar-pre-autorizacao-multi? paciente-particular))
+  (println (deve-assinar-pre-autorizacao-multi? paciente-plano)))
+
+; pedido { :paciente paciente :valor valor :procedimento procedimento }
+(defn tipo-de-autorizador
+  [pedido]
+  (let [paciente  (get pedido :paciente)
+        situacao  (get paciente :situacao)
+        urgencia? (= :urgente situacao)]
+    (if urgencia?
+      :sempre-autorizado
+      (class paciente))))
+
+; passamos para o `defmulti` a função `tipo-de-autorizador`
+; o retorno dela será o "tipo" no qual nós chamaremos a função
+; `deve-assinar-pre-autorizacao-pedido?`
+; Contudo, para isso, devemos ter um `defmethod`
+; associado a este tipo de retorno.
+(defmulti deve-assinar-pre-autorizacao-pedido? tipo-de-autorizador)
+
+; quando `tipo-de-autorizador` retornar `:sempre-autorizado`
+; é a função a seguir que será executada.
+(defmethod deve-assinar-pre-autorizacao-pedido? :sempre-autorizado
+  [pedido]
+  false)
+
+; quando `tipo-de-autorizador` retornar `PacienteParticular`
+; é a função a seguir que será executada.
+(defmethod deve-assinar-pre-autorizacao-pedido? PacienteParticular
+  [pedido]
+  (>= (get pedido :valor) 50))
+
+; quando `tipo-de-autorizador` retornar `PacientePlanoDeSaude`
+; é a função a seguir que será executada.
+(defmethod deve-assinar-pre-autorizacao-pedido? PacientePlanoDeSaude
+  [pedido]
+  (let [procedimento (get pedido :procedimento)
+        paciente (get pedido :paciente)
+        plano (get paciente :plano)]
+    (not (some #(= % procedimento) plano))))
+
+
+(let [paciente-particular (->PacienteParticular 1 "Bob" :normal "11/11/11")
+      paciente-particular-2 (->PacienteParticular 1 "Tita" :urgente "11/11/11")
+      paciente-plano      (->PacientePlanoDeSaude 2 "João" "10/11/12" :urgente [:raio-x :sangue])
+      paciente-plano-2      (->PacientePlanoDeSaude 2 "Joãozinho" "10/11/12" :normal [:raio-x :sangue])]
+
+  (println "\n\n\n### Estendendo com defmulti e defmethod ###")
+  (println "## Misturando valores e classes ##")
+  (println (deve-assinar-pre-autorizacao-pedido? {:paciente paciente-particular
+                                                  :valor 1000
+                                                  :procedimento :coleta-de-sangue}))
+  (println (deve-assinar-pre-autorizacao-pedido? {:paciente paciente-particular-2
+                                                  :valor 1000
+                                                  :procedimento :coleta-de-sangue}))
+  (println (deve-assinar-pre-autorizacao-pedido? {:paciente paciente-plano
+                                                  :valor 1000
+                                                  :procedimento :coleta-de-sangue}))
+  (println (deve-assinar-pre-autorizacao-pedido? {:paciente paciente-plano-2
+                                                  :valor 1000
+                                                  :procedimento :coleta-de-sangue})))
