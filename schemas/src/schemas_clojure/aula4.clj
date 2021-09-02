af0cb7c3999ca62028b5ac477a2f9a674ae69134
allan-mlpe <allan_mlpe@hotmail.com>
2021-09-02 14:04:25 -0300

    Validando mapas

--- schemas/src/schemas_clojure/aula4.clj
+++ schemas/src/schemas_clojure/aula4.clj
@@ -16,5 +16,19 @@
 (pprint (s/validate Paciente {:id 1, :nome "Bob", :plano [:raio-x, :coleta-de-sangue]}))
 (pprint (s/validate Paciente {:id 1, :nome "Pito", :plano [], :nascimento "20/12/20"}))
 
+; validação de mapas
+(def Pacientes
+  {PosInt Paciente})
 
+(let [paciente {:id 1, :nome "Joca", :plano [:raio-x]}
+      paciente-2 {:id 1, :nome "Bob", :plano [:raio-x]}
+      paciente-3 {:id 1, :nome "Tita"}]
+  (println "\n# Validando mapas")
+  (pprint (s/validate Pacientes {15 paciente}))
+  (pprint (s/validate Pacientes {15 paciente 16 paciente-2}))
+  (pprint (s/validate Pacientes {}))
+
+  ;(pprint (s/validate Pacientes {-3 paciente-2})) ; falha, pois a chave não é um inteiro positivo
+  ;(pprint (s/validate Pacientes {19 paciente-3})) ; falha, pois paciente-3 não é um paciente
+  )
 
