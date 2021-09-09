9f6cb889b411caf65cf465273cf154ccd23d639b
allan-mlpe <allan_mlpe@hotmail.com>
2021-09-09 10:16:13 -0300

    Trabalhando com mais opções de geração de amostras de testes

--- geradores_e_testes_de_propriedade/src/hospital/core.clj
+++ geradores_e_testes_de_propriedade/src/hospital/core.clj
@@ -18,3 +18,16 @@
 (println (gen/sample (gen/vector gen/nat 6)))               ; gera amostra de 10 vetores de tamanho fixo igual a 6
 (println (gen/sample (gen/vector gen/nat 1 5)))             ; gera amostra de 10 vetores de tamanho variando de 1 a 5
 (println (gen/sample (gen/large-integer* {:min 1})))        ; gera amostra de números aleatórios, onde o menor número gerado será 1
+
+(println (gen/sample(gen/not-empty gen/string-alphanumeric))) ; gera amostra com strings alfa-numéricas NÃO VAZIAS
+
+; mapeando funções para um gerador
+;; exemplo: quero aplicar um join em um vetor de strings
+(def nome-aleatorio
+  (gen/fmap clojure.string/join                             ; mapeia a função join
+            (gen/vector gen/char-alphanumeric 5 10)))       ; em um gerador de vetor com 5 a 10 caracteres alfa-numéricos
+; então, podemos chamar o "novo gerador" normalmente
+(println (gen/sample nome-aleatorio))
+
+; gera uma amostra de um conjunto de valores pré-definidos
+(println (gen/sample (gen/elements [:a :b :c :casa])))      ; gera uma amostra aleatória com um dos valores que passamos no array
